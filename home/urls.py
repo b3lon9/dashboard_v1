a26a1c59edbdf5d1687555f4f85f2b0797dc1bd8
HimeYuyuko <jang728898@gmail.com>
2022-12-13 12:02:07 +0900

    Log out tap

--- home/urls.py
+++ home/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^.*\.*', views.pages, name='pages'),
 ]
+
+# <a class="dropdown-item" href="{% url 'account_logout' %}"><i class="fa fa-power-off m-r-5 m-l-5"></i> Logout</a>
