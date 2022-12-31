1a8f1f2300416cfcb4b37c096f138bbffbac073c
SeokWon Kwon <45963729+dhjkl123@users.noreply.github.com>
2022-12-31 16:02:48 +0900

    20221231 ksw - 함수 분리

--- functions/pos_neg_all.py
+++ functions/pos_neg_all.py
@@ -2,70 +2,30 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 import requests
 import re
-import easyocr
-import sys
-import os
+
 import json
 import pickle
 import urllib
-import pytesseract
-from urllib import request
-from PIL import Image
-from io import BytesIO
+
 import numpy as np
 from tqdm import tqdm
 import urllib.request
 import tensorflow as tf
 from transformers import BertTokenizer, TFBertForSequenceClassification
+from .crawling_func import *
 
 def crawling_news(keyword:str, begin:int,cnt:int) :
-    def makePgNum(num):
-        if num == 1:
-            return num
-        elif num == 0:
-            return num+1
-        else:
-            return num+9*(num-1)
-
-    def makeUrl(search, start_pg, end_pg):
-        if start_pg == end_pg:
-            start_page = makePgNum(start_pg)
-            url = "https://search.naver.com/search.naver?where=news&query=" + search + "&start=" + str(start_page)
-            return url
-        else:
-            urls = []
-            for i in range(start_pg, end_pg + 1):
-                page = makePgNum(i)
-                url = "https://search.naver.com/search.naver?where=news&query=" + search + "&start=" + str(page)
-                urls.append(url)
-            return urls    
-
-    def news_attrs_crawler(articles,attrs):
-        attrs_content=[]
-        for i in articles:
-            attrs_content.append(i.attrs[attrs])
-        return attrs_content
 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/98.0.4758.102"}
 
-    def articles_crawler(url):
-        #html 불러오기
-        original_html = requests.get(i,headers=headers)
-        html = bs(original_html.text, "html.parser")
-
-        url_naver = html.select("div.group_news > ul.list_news > li div.news_area > div.news_info > div.info_group > a.info")
-        url = news_attrs_crawler(url_naver,'href')
-        return url
-
-    url = makeUrl(keyword,begin,cnt)
+    urls = makeUrl(keyword,begin,cnt)
 
     news_titles = []
     news_url =[]
     news_contents =[]
 
-    for i in url:
-        url = articles_crawler(url)
-        news_url.append(url)
+    for url in urls:
+        news_url.append(articles_crawler(url,headers))
 
     def makeList(newlist, content):
         for i in content:
@@ -232,69 +192,18 @@ def crawling_blog(keyword:str):
     for i in range(int(display_num)):
         links.append(items[i]['link']) # 링크 하나씩 저장
         titles.append(items[i]['title']) # 제목 하나씩 저장
-
+        
     # 제목 다듬기 (<b></b>태그 제거)
     for i in range(len(titles)):
         titles[i] = titles[i].replace('<b>','')
         titles[i] = titles[i].replace('</b>','')
-
-    # iframe태그로 인해 본문을 못긁어오는 현상 발생.
-    # iframe태그에 명시된 blog url을 뽑아와서 blog로 바로 연결할 수 있도록 url조립
-    def delete_iframe(url): 
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
-        res = requests.get(url, headers=headers)
-        res.raise_for_status() # 문제시 프로그램 종료
-        soup = bs(res.text, "html.parser") 
-
-        src_url = "https://blog.naver.com/" + soup.iframe["src"]
-        return src_url
 
     # 조립된 블로그 링크들을 담는 리스트
     blog_links = []
 
     for j in range(len(links)):
         blog_links.append(delete_iframe(links[j]))
-
-    # 수집한 블로그 링크를 하나씩 들어가서 본문을 긁어오는 함수 
-    def text_scraping(url):
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
-        res = requests.get(url, headers=headers)
-        soup = bs(res.text, "html.parser") 
-
-        # 본문이 담겨있는 태그 : se-main-container
-        if soup.find("div", attrs={"class":"se-main-container"}):
-            text = soup.find("div", attrs={"class":"se-main-container"}).get_text()
-            text = text.replace("\n","") #공백 제거
-            return text
-
-        elif soup.find("div", attrs={"id":"postViewArea"}):
-            text = soup.find("div", attrs={"id":"postViewArea"}).get_text()
-            text = text.replace("\n","") 
-            return text
-        else:
-            return "네이버 블로그는 맞지만, 확인불가"
         
-    # 블로그 링크로 마지막 이미지의 문장을 탐지한 결과를 불러오는 함수
-    def image_scraping(url):
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
-        res = requests.get(url, headers=headers)
-        soup = bs(res.text, "html.parser") 
-        
-        img = soup.find_all('img', {'class' : ['se-sticker-image', 'se-image-resource']})
-
-        try:
-            res = request.urlopen(img[-1]["data-lazy-src"]).read()
-            result_l = pytesseract.image_to_string(Image.open(BytesIO(res)), lang='kor+eng')
-            return result_l
-        except KeyError:
-            res = request.urlopen(img[-1]["src"]).read()
-            result_l = pytesseract.image_to_string(Image.open(BytesIO(res)), lang='kor+eng')
-            return result_l
-        except AttributeError:
-            return ""
-        except IndexError:
-            return ""
-
     # 각 블로그들의 본문들을 담는 리스트
     contents = []
     
@@ -305,7 +214,7 @@ def crawling_blog(keyword:str):
         contents.append(text_scraping(blog_links[o]))
         images.append(image_scraping(blog_links[o]))
         # print(contents[o])
-
+        
     print("<<-- 블로그 크롤링 끝 -->>") 
 
     cate = 'blog'
@@ -410,9 +319,13 @@ def AD_filtering(keyword):
     for i in range(len(blog_rmv_target)):
         blog_df.drop(blog_rmv_target[i], axis=0, inplace=True)
         
+    print('blog 광고 : ', len(blog_rmv_target))
+        
     if len(cafe_rmv_target) != 0 :
         for i in range(len(cafe_rmv_target)):
             cafe_df.drop(cafe_rmv_target[i], axis=0, inplace=True)
+            
+    print('cafe 광고 : ', len(cafe_rmv_target))
         
     # 블로그 df에서 'img'만 뽑아서 리스트화
     blog_img_lst = blog_df['img'].to_list()
